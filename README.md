dc368d14132c55c8466786fc04e0b8116ee00a8c
Pierre Josselin <contact@pierre-josselin.fr>
2022-04-01 11:26:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# PHP REST API template
+
+A simple REST API template for PHP.
+
+## Getting started
+
+### Prerequisites
+
+- PHP
+- Apache (mod_rewrite)
+- Composer
+
+### Installation
+
+Install composer dependencies.
+
+```
+composer install
+```
---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+error_reporting(E_ALL & ~E_NOTICE);
+ini_set("display_errors", 0);
+
+define("ROOT_DIRECTORY_PATH", str_replace(["/", "\\"], DIRECTORY_SEPARATOR, dirname(realpath(__FILE__), 2)));
+
+require(implode(DIRECTORY_SEPARATOR, [ROOT_DIRECTORY_PATH, "vendor", "autoload.php"]));
+
+$dotenv = Dotenv\Dotenv::createImmutable(ROOT_DIRECTORY_PATH);
+$dotenv->load();
+$dotenv->required("DEBUG")->isBoolean();
+
+ini_set("display_errors", intval(filter_var($_ENV["DEBUG"], FILTER_VALIDATE_BOOLEAN)));
