28b8b92724c7d3f22923484c8ef2864da0d8d07a
Pierre Josselin <contact@pierre-josselin.fr>
2022-04-14 10:35:04 +0200

    Add destruct magic method to Response class

--- Core/Response.php
+++ Core/Response.php
@@ -8,6 +8,19 @@ class Response
     private array $headers = [];
     private string $body;
 
+    public function __destruct()
+    {
+        foreach ($this->headers as $name => $value) {
+            header("{$name}: {$value}");
+        }
+        if (isset($this->statusCode)) {
+            http_response_code($this->statusCode);
+        }
+        if (isset($this->body)) {
+            echo ($this->body);
+        }
+    }
+
     public function setStatusCode(int $statusCode): void
     {
         $this->statusCode = $statusCode;
@@ -30,15 +43,6 @@ class Response
 
     public function send(): void
     {
-        foreach ($this->headers as $name => $value) {
-            header("{$name}: {$value}");
-        }
-        if (isset($this->statusCode)) {
-            http_response_code($this->statusCode);
-        }
-        if (isset($this->body)) {
-            echo ($this->body);
-        }
         exit;
     }
 }
